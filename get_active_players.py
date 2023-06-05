5f46542b2e656d44c5a87a767779eb00d86e0d63
squireaa <squireaa1@gmail.com>
2023-06-05 18:07:36 -0400

    started to get active players

--- get_active_players.py
+++ get_active_players.py
@@ -5,20 +5,59 @@ from nhlstats.formatters import csv
 from format_team_name import get_three_letter_code
 
 games = list_games(start_date="2017-10-04", end_date="2022-12-31")
-teams_list = set()
+teams_list = list()
 for i, game in enumerate(games):
     date = game['date']
     team1 = game['away_team']
     team2 = game['home_team']
-    game_id = game['game_id']
+    game_id = str(game['game_id'])
     
-    teams_list.add(get_three_letter_code(team1))
+    if get_three_letter_code(team1) != None and get_three_letter_code(team1) != None:
+        teams_list.append([date, get_three_letter_code(team1), get_three_letter_code(team2), game_id])
 
-df = pd.read_csv("C:/Users/Owner/Desktop/cs stuff/Open Source/nhl-nn-sports-betting/2022odds.csv")
+df = pd.DataFrame(columns=['my_id', 'player_list'])
+row_index = 0
 
-for i in range(200):
-    team = df['Team'].iloc[i]
-    teams_list.add(get_three_letter_code(team))
+# ------------START LOOP SECTION---------------------------
+#for i in range(len(teams_list)):
+for i in range(5):
+    gameid = teams_list[i][3]
+    player_list = set()
+    team1_list = []
+    team2_list = []
 
-teams_list.discard(None)
-print(len(teams_list))
+    shifts = pd.DataFrame(list_shifts(gameid))
+
+    for i in range(len(shifts)):
+        player_code = "{2} {0} {1}".format(shifts['first_name'][i], shifts['last_name'][i], shifts['team_abbreviation'][i])
+        player_list.add(player_code)
+
+    sorted_list = list(player_list)
+    sorted_list.sort()
+
+    for name in sorted_list:
+        team1_letters = sorted_list[0][:3]
+        if name[:3] == team1_letters:
+            team1_list.append(name)
+        else:
+            team2_list.append(name)
+
+    team1_letters = team1_list[0][:3]
+    print(teams_list[i][1])
+    print(team1_letters)
+    if str(teams_list[i][1]) == str(team1_letters):
+        df.at[row_index, 'my_id'] = "{0}-{1}".format(teams_list[i][1], teams_list[i][0])
+        df.at[row_index, 'player_list'] = team1_list
+        row_index += 1
+        df.at[row_index, 'my_id'] = "{0}-{1}".format(teams_list[i][2], teams_list[i][0])
+        df.at[row_index, 'player_list'] = team2_list
+        row_index += 1
+    else:
+        df.at[row_index, 'my_id'] = "{0}-{1}".format(teams_list[i][2], teams_list[i][0])
+        df.at[row_index, 'player_list'] = team2_list
+        row_index += 1
+        df.at[row_index+1, 'my_id'] = "{0}-{1}".format(teams_list[i][1], teams_list[i][0])
+        df.at[row_index+1, 'player_list'] = team1_list
+        row_index += 1
+
+print(df)
---
+++ tests/format_name_test.py
@@ -0,0 +1,25 @@
+from nhlstats import list_games
+from nhlstats import list_shifts
+import pandas as pd
+from nhlstats.formatters import csv
+from format_team_name import get_three_letter_code
+
+games = list_games(start_date="2017-10-04", end_date="2022-12-31")
+teams_list = set()
+for i, game in enumerate(games):
+    date = game['date']
+    team1 = game['away_team']
+    team2 = game['home_team']
+    game_id = game['game_id']
+
+    teams_list.add(get_three_letter_code(team1))
+
+df = pd.read_csv(
+    "C:/Users/Owner/Desktop/cs stuff/Open Source/nhl-nn-sports-betting/2022odds.csv")
+
+for i in range(200):
+    team = df['Team'].iloc[i]
+    teams_list.add(get_three_letter_code(team))
+
+teams_list.discard(None)
+print(len(teams_list))
